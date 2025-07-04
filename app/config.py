31b4685738ec804de3934b368c11fca56eb458cb
esmrzv <esmurziev.1999@mail.ru>
2025-07-04 15:37:56 +0300

    first_commit

---
+++ app/config.py
@@ -0,0 +1,15 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+    ADMINS_ID: list[int]
+    FORMAT_LOG: str = "{time:YYYY-MM-DD at HH:mm:ss.SSS}  |  {level} | {message}"
+    LOG_ROTATION: str = "10 MB"
+    DB_URL: str = "sqlite+aiosqlite:///data/db.sqlite3"
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)),"..", ".env"),
+    )
+
+settings = Settings()
---
+++ app/main.py
@@ -0,0 +1,5 @@
+from aiogram import Bot, Dispatcher, F
+
+BOT_TOKEN = '6915846876:AAGyf6KAGXvQfp1c9KVwrkUR25bxtKAEzeM'
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
